905edcaeda34d851ce3b8555f9d8e9fd9b120b22
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2021-05-31 11:25:01 +0300

    переделано задание с сигналами ОС

--- lesson4/ossignal/main.go
+++ lesson4/ossignal/main.go
@@ -62,22 +62,4 @@ func main() {
 	time.Sleep(time.Second)
 	// завершить приложеие
 	os.Exit(-1)
-
-	// создать контекст, который закроет свой канал done() при поступлении сигнал SIGINT и SIGTERM
-	ctxSig, cancelSig := signal.NotifyContext(context.Background(), os.Interrupt, Terminate)
-	// в конце функции main вызвать функцию завершения
-	defer cancelSig()
-
-	// создать контектс с таймаутом
-	ctxTimeoOut, cancelTimeoOut := context.WithTimeout(context.Background(), time.Second)
-	// в конце функции main вызвать функцию завершения
-	defer cancelTimeoOut()
-
-	// ожидать поступления сигнала SIGTERM или завершения таймаута
-	select {
-	case <-ctxSig.Done():
-		fmt.Printf("Поступил сигнал. Причина завершения контекста: %v\n", ctxSig.Err())
-	case <-ctxTimeoOut.Done():
-		fmt.Printf("Наступил таймаут. Причина завершения контекста:  %v\n", ctxTimeoOut.Err())
-	}
 }
