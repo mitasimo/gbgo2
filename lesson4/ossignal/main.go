672da6acfd3c71f9a677262176dad0e9e959f1f9
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2021-05-31 11:23:43 +0300

    переделано задание с сигналами ОС

--- lesson4/ossignal/main.go
+++ lesson4/ossignal/main.go
@@ -14,6 +14,55 @@ var (
 )
 
 func main() {
+
+	// создать канал для получиния сигналов ОС
+	chanSig := make(chan os.Signal)
+	// настроить канал на получение сигналов SIGINT и SIGTERM
+	signal.Notify(chanSig, os.Interrupt, Terminate)
+
+	// создать контекст для завершения грутин
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// первая горутина
+	go func(c context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				// при закрытии канала сообщить о завершении горуиы
+				fmt.Println("Горутина 1 завершена")
+				return
+			default:
+				// заснуть на 2 секунды
+				time.Sleep(time.Second * 2)
+			}
+
+		}
+	}(ctx)
+
+	// вторая горутина
+	go func(c context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				// при закрытии канала сообщить о завершении горуиы
+				fmt.Println("Горутина 2 завершена")
+				return
+			default:
+				// заснуть на 2 секунды
+				time.Sleep(time.Second * 2)
+			}
+		}
+	}(ctx)
+
+	// ожидать сигнал ОС
+	<-chanSig
+	// завершить контекст
+	cancel()
+	// подождать завершения горутин 1 секунда
+	time.Sleep(time.Second)
+	// завершить приложеие
+	os.Exit(-1)
+
 	// создать контекст, который закроет свой канал done() при поступлении сигнал SIGINT и SIGTERM
 	ctxSig, cancelSig := signal.NotifyContext(context.Background(), os.Interrupt, Terminate)
 	// в конце функции main вызвать функцию завершения
