c6a2e9abd2c8fed89aee56815872c39d1006b934
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2021-06-01 15:33:49 +0300

    WaitGroup

---
+++ lesson5/wait_group/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i += 2 {
+		go Do(&wg, 20*i, i)
+	}
+
+	wg.Wait()
+	fmt.Println("Программа завершена")
+
+}
+
+func Do(wg *sync.WaitGroup, millisecondToSleep, num int) {
+	wg.Add(1)
+	defer wg.Done()
+
+	fmt.Printf("Выполняется горутина %2d\n", num)
+	time.Sleep(time.Millisecond * time.Duration(millisecondToSleep))
+}
