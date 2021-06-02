da06a099230c2dfbe00ba520056d36a4342a7d6a
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2021-06-02 17:23:29 +0300

    begin benchmark task

---
+++ lesson5/bencmark/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkMutexW10R90(b *testing.B) {
+	//var me sync.Mutex
+
+}
+
+func BenchmarkMutexW50R50(b *testing.B) {
+	//var me sync.Mutex
+
+}
+
+func BenchmarkMutexW90R10(b *testing.B) {
+	//var me sync.Mutex
+
+}
+
+func BenchmarkRWMutexW10R90(b *testing.B) {
+	//var me sync.RWMutex
+
+}
+
+func BenchmarkRWMutexW50R50(b *testing.B) {
+	//var me sync.RWMutex
+
+}
+
+func BenchmarkRWMutexW90R10(b *testing.B) {
+	//var me sync.RWMutex
+
+}
