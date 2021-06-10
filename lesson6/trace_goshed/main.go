28f478c8e75eb3f4143382b5a9426a2def1badf6
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2021-06-10 11:09:04 +0300

    add trace_goshed

---
+++ lesson6/trace_goshed/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+	"runtime"
+	"runtime/trace"
+	"sync"
+)
+
+func main() {
+	var (
+		wg sync.WaitGroup
+	)
+
+	// начать запись Trace
+	trace.Start(os.Stderr)
+	defer trace.Stop()
+
+	for i := 0; i < 16; i++ {
+		// увеличить счетчик
+		wg.Add(1)
+		go func() {
+			// при завершении функции уменьшить счетчик
+			defer wg.Done()
+
+			for j := 0; j < 1000000; j++ {
+				if j%100 == 0 { // на каждом 100м элементе
+					// попросить планировщик прекратить выполнение потока и проверить,
+					// нет ли других потоков в состоянии готовности
+					runtime.Gosched()
+				}
+			}
+		}()
+	}
+
+	// дождасться завершения всех горутин
+	wg.Wait()
+}
